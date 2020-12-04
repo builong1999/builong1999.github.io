e3f9c9c62b2d3ccc4433c62e967bb9e884027843
Long Bui <long.builong.bui@hcmut.edu.vn>
2020-12-04 13:00:55 +0700

    ok

---
+++ precache-manifest.23cad8891b2664d920a8fe6c2c40a7db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680dfae159d3177ac3d465e0a643ce39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02833661df59dedb52c5",
+    "url": "/static/css/main.3a18c446.chunk.css"
+  },
+  {
+    "revision": "678a82a53ac62506b289",
+    "url": "/static/js/2.19bcd366.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.19bcd366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02833661df59dedb52c5",
+    "url": "/static/js/main.69c826de.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  },
+  {
+    "revision": "5fed09ec50b4d016184f0f7a8d2a8072",
+    "url": "/static/media/avatar.5fed09ec.jpg"
+  },
+  {
+    "revision": "27fd47793e93578513b6ded909842a20",
+    "url": "/static/media/background-img.27fd4779.jpg"
+  },
+  {
+    "revision": "ee958f158b5b6c365cff9d294573dde5",
+    "url": "/static/media/cv.ee958f15.pdf"
+  },
+  {
+    "revision": "4e24837e4651da4be682e18bf43741c7",
+    "url": "/static/media/framework.4e24837e.png"
+  }
+]);
